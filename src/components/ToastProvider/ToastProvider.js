46806b77951158eb467025649cf20c8ac6a6ae47
OkeyIke <benjaminwerner16@gmail.com>
2024-04-19 03:35:44 +0200

    feat(ToastProvider): Implement keyboard shortcut for dismissing all toasts simultaneously.

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -5,6 +5,19 @@ export const ToastContext = React.createContext()
 function ToastProvider({ children }) {
   const [toasts, setToasts] = React.useState([])
 
+  React.useEffect(() => {
+    function handleKeydown(event) {
+      if (event.code === 'Escape') {
+        setToasts([])
+      }
+    }
+    window.addEventListener('keydown', handleKeydown)
+
+    return () => {
+      window.removeEventListener('keydown', handleKeydown)
+    }
+  }, [])
+
   function createToast(message, variant) {
     const nexToasts = [...toasts, { id: crypto.randomUUID(), message, variant }]
     setToasts(nexToasts)
